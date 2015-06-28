66b8dbf465bf00159f058452e1dd6b8bbe3a96a3
alreyes1 <alreyes1@icloud.com>
2015-06-27 23:15:42 -0400

    First Commit

---
+++ HelloWorld Watchkit WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  HelloWorld Watchkit WatchKit Extension
+//
+//  Created by Angel Reyes on 6/27/15.
+//  Copyright (c) 2015 Angel Reyes. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
